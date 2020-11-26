7d7a44abca457b59c6886fee080d03a3e3273762
Amy Rombuk <arombuk@gmail.com>
2020-11-27 09:38:25 +1000

    Added indenting to components

--- src/App.js
+++ src/App.js
@@ -23,42 +23,42 @@ class App extends Component {
       console.log("form data")      
       // Posting that data to our API     
       axios.post('http://localhost:3330/login-patient', formData)
+    })
+  }
+
+  getDoctorLoginData = () => {
+    // Find doctor login form 
+    const loginForm = document.querySelector('.login-doctor-form') 
+
+    // Process the sumbission of the form 
+    loginForm.addEventListener('submit', function(event) {     
+      // Stopping the normal action of the form so that we can handle things here     
+      event.preventDefault()     
+      console.log(event.target)     
+      console.log("Submitting the login form")         
+      var formData = new FormData(loginForm)         
+      console.log("form data")      
+      // Posting that data to our API     
+      axios.post('http://localhost:3330/login-doctor', formData)
   })
-}
+  }
 
-getDoctorLoginData = () => {
-  // Find doctor login form 
-  const loginForm = document.querySelector('.login-doctor-form') 
+  getAdminLoginData = () => {
+    // Find admin login form 
+    const loginForm = document.querySelector('.login-admin-form') 
 
-  // Process the sumbission of the form 
-  loginForm.addEventListener('submit', function(event) {     
-    // Stopping the normal action of the form so that we can handle things here     
-    event.preventDefault()     
-    console.log(event.target)     
-    console.log("Submitting the login form")         
-    var formData = new FormData(loginForm)         
-    console.log("form data")      
-    // Posting that data to our API     
-    axios.post('http://localhost:3330/login-doctor', formData)
-})
-}
-
-getAdminLoginData = () => {
-  // Find admin login form 
-  const loginForm = document.querySelector('.login-admin-form') 
-
-  // Process the sumbission of the form 
-  loginForm.addEventListener('submit', function(event) {     
-    // Stopping the normal action of the form so that we can handle things here     
-    event.preventDefault()     
-    console.log(event.target)     
-    console.log("Submitting the login form")         
-    var formData = new FormData(loginForm)         
-    console.log("form data")      
-    // Posting that data to our API     
-    axios.post('http://localhost:3330/login-admin', formData)
-})
-}
+    // Process the sumbission of the form 
+    loginForm.addEventListener('submit', function(event) {     
+      // Stopping the normal action of the form so that we can handle things here     
+      event.preventDefault()     
+      console.log(event.target)     
+      console.log("Submitting the login form")         
+      var formData = new FormData(loginForm)         
+      console.log("form data")      
+      // Posting that data to our API     
+      axios.post('http://localhost:3330/login-admin', formData)
+    })
+  }
 
   getAppointData = () => {
     // Find appointment form 
@@ -74,8 +74,8 @@ getAdminLoginData = () => {
       console.log("form data")      
       // Posting that data to our API     
       axios.post('http://localhost:3330/add-appointment', formData)
-  })
-}
+    })
+  }
 
   getConsultData = () => {
     // Find consultation form 
@@ -91,7 +91,7 @@ getAdminLoginData = () => {
       console.log("form data")      
       // Posting that data to our API     
       axios.post('http://localhost:3330/add-consultation', formData)
-  })
+    })
   }
 
   addAdmin = () => {
@@ -108,7 +108,7 @@ getAdminLoginData = () => {
       console.log("form data")      
       // Posting that data to our API     
       axios.post('http://localhost:3330/add-admin', formData)
-  })
+    })
   }
 
   addDoctor = () => {
@@ -125,7 +125,7 @@ getAdminLoginData = () => {
       console.log("form data")      
       // Posting that data to our API     
       axios.post('http://localhost:3330/add-doctor', formData)
-  })
+    })
   }
 
   addPatient = () => {
@@ -142,53 +142,47 @@ getAdminLoginData = () => {
       console.log("form data")      
       // Posting that data to our API     
       axios.post('http://localhost:3330/add-patient', formData)
-  })
+    })
   }
 
   render() {
     console.log("This page is rendering")
     return(
       <div className= "login-page">
-      <BrowserRouter>
-      <header>
-         <h1>MAC</h1>
-         <h2>Medical Appointments & Consultations</h2>
-      </header>
-      
-      <div className="nav-bar">
-         <a href="#about">About</a>
-         <a href="#contacts">Contacts</a>
-         <a href="#history">History</a>
-         <div className="dropdown">
-            <button className="dropbtn">Login 
-               <i className="fa fa-caret-down"></i>
-            </button>
-           <div className="dropdown-content">
-              <Link to='/admin-login'><button>Login as an Admin</button></Link>
-              <Link to='/doctor-login'><button>Login as a Doctor</button></Link>
-              <Link to='/patient-login'><button>Login as a Patient</button></Link>
-           </div> 
-         </div>
-      </div>
+        <BrowserRouter>
+          <header>
+            <h1>MAC</h1>
+            <h2>Medical Appointments & Consultations</h2>
+          </header>
+          
+          <div className="nav-bar">
+            <a href="#about">About</a>
+            <a href="#contacts">Contacts</a>
+            <a href="#history">History</a>
+            <div className="dropdown">
+                <button className="dropbtn">Login 
+                  <i className="fa fa-caret-down"></i>
+                </button>
+                <div className="dropdown-content">
+                    <Link to='/admin-login'><button>Login as an Admin</button></Link>
+                    <Link to='/doctor-login'><button>Login as a Doctor</button></Link>
+                    <Link to='/patient-login'><button>Login as a Patient</button></Link>
+                </div> 
+            </div>
+          </div>
 
-      <Switch>
-          <Route exact path="/admin-login">
-            <AdminLogin getAdmData={this.getAdminLoginData} newAdm={this.addAdmin} newDoc={this.addDoctor} newPat={this.addPatient}/>
-          </Route>
-          <Route exact path="/doctor-login">
-            <DoctorLogin getDocData={this.getDoctorLoginData} consultData={this.getConsultData}/>
-          </Route>
-          <Route exact path="/patient-login">
-<<<<<<< HEAD
-            <PatientLogin appointmentData={this.getAppointData}/>
-=======
-            <PatientLogin getPatData={this.getPatientLoginData} appointData= {this.getAppointData}/>
->>>>>>> fcdfa3fd1f70b434bdc477defa467bcd136efd84
-          </Route>
-  
-        </Switch>
-      
-      </BrowserRouter>
+          <Switch>
+            <Route exact path="/admin-login">
+              <AdminLogin getAdmData={this.getAdminLoginData} newAdm={this.addAdmin} newDoc={this.addDoctor} newPat={this.addPatient}/>
+            </Route>
+            <Route exact path="/doctor-login">
+              <DoctorLogin getDocData={this.getDoctorLoginData} consultData={this.getConsultData}/>
+            </Route>
+            <Route exact path="/patient-login">
+              <PatientLogin getPatData={this.getPatientLoginData} appointData= {this.getAppointData}/>
+            </Route>
+          </Switch>
+        </BrowserRouter>
       </div>
     )
   }
